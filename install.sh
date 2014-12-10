f6f8b5ae34b73b7ac10de3bd00660e88705e62ff
Raymond Chan <ucamrc@users.noreply.github.com>
2014-12-10 13:08:01 +0000

    first rough attempt at automating deployment (not tested) - based on NotesToSelf201412 branch .travis.yml

---
+++ install.sh
@@ -0,0 +1,50 @@
+mkdir -p ~/build/CUL-DigitalServices
+
+  # Install Grasshopper
+echo 'Installing Grasshopper'
+cd ~/build/CUL-DigitalServices
+git clone --branch master --depth=1 git://github.com/CUL-DigitalServices/grasshopper.git ./grasshopper
+cd ./grasshopper
+npm install --production
+
+  # Configure postgres
+# Done in previous script as it needs sudo
+
+echo "config.db.database = 'grasshoppertest';" >> config.js
+
+  # Install Grasshopper UI
+echo 'Installing Grasshopper UI'
+cd ~/build/CUL-DigitalServices
+git clone --branch master --depth=1 git://github.com/CUL-DigitalServices/grasshopper-ui.git ./grasshopper-ui
+cd ~/build/CUL-DigitalServices/grasshopper-ui
+npm install
+
+
+##### DO THIS MANUALLY ######
+
+# this is where grunt-cli seems to be needed
+# TODO: find out more about how this grunt task works
+# TODO: review the apache config contents and understand how they work, e.g. log location, parameterise things, how much is 2.4-specific, how much is grasshopper, how much is grasshopper-ui
+# work out how best to do the multiple VirtualHosts in production (admin and tenants on separate interfaces?)
+# e.g. for my own testing purposes:
+#sudo vim /usr/local/apache2/conf/sites-enabled/app_admin.conf # edit 127.0.0.1:80 to *:80 to make it work on 192.168.56.56
+#sudo vim /usr/local/apache2/conf/sites-enabled/app_timetable.conf # edit 127.0.0.2:80 to *:80 to make it work on 192.168.56.56 , add ServerName directives for each of those in /etc/hosts above
+#sudo /usr/local/apache2/bin/apachectl restart
+#  - grunt configApache
+#  - sudo cp ./target/optimized/apache/httpd.conf /usr/local/apache2/conf/httpd.conf
+#  - sudo cp -R ./target/optimized/apache/app_*.conf /usr/local/apache2/conf/sites-enabled
+
+
+###### Start Apache ######
+###  - sudo /usr/local/apache2/bin/apachectl start
+
+##### also start the grasshopper server (as per its README.md):
+### node app
+
+
+
+
+###### PUT THIS IN CRON #######
+
+## cd build/CUL-DigitalServices/grasshopper-ui
+## git pull
---
+++ sudo-install.sh
@@ -0,0 +1,103 @@
+# TODO: guard against being run as non-root
+
+##### Installing Node.js ...
+
+#language: node_js
+#node_js:
+#  - "0.10.33"
+# grasshopper uses - "0.10.30" ??
+# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
+# https://deb.nodesource.com/setup magically (scarily!) configures apt-get to just do the right thing (urgh) - but nicer than non apt ??
+# maybe do this locally/fork it locally etc - although it will be different from travis.
+# TRY Paste this at end of sources.list for now
+
+echo 'deb https://deb.nodesource.com/node precise main' >> /etc/apt/sources.list
+echo 'deb-src https://deb.nodesource.com/node precise main' >> /etc/apt/sources.list 
+curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
+apt-get update
+apt-cache showpkg nodejs # shows 0.10.33 at the moment
+apt-get install nodejs # without the above fixes, actually installs 0.6.12
+
+# Travis log appears to use "nvm install 0.10.33"
+
+# http://achinth.com/post/58263924087/installing-node-js-using-nvm-on-ubuntu
+# https://raw.githubusercontent.com/creationix/nvm/master/install.sh # argh more scary run stuff off the web as root
+
+
+###### PostgreSQL
+
+#addons:
+#  postgresql: "9.3"
+# Is "addon" the same as just psql or is it an addon for node.js?
+
+apt-get install postgresql # actually installs 9.1 ??
+
+# [start postgresql is built in]
+
+sudo -u postgres psql template1 -c 'CREATE USER grasshopper WITH PASSWORD '\''grasshopper'\'';'
+sudo -u postgres psql template1 -c 'CREATE DATABASE grasshopper;'
+sudo -u postgres psql template1 -c 'CREATE DATABASE grasshoppertest;'
+sudo -u postgres psql template1 -c 'GRANT ALL PRIVILEGES ON DATABASE grasshopper TO grasshopper;'
+sudo -u postgres psql template1 -c 'GRANT ALL PRIVILEGES ON DATABASE grasshoppertest TO grasshopper;'
+
+
+###### Git
+apt-get install git
+
+  # Prepare the machine
+apt-get update
+# mostly gcc and make ; probably similar to stuff already on Travis so possibly superfluous
+# But a bigger question (cf SG??) is whether it's appropriate to build Apache and dependencies from scratch?
+# If we are going to build from source, let's pick a more official source than lib.gblearn.com
+# see http://httpd.apache.org/docs/2.4/install.html (as mentioned in README.md)
+## it explains the dependencies, and has more official mirrors than lib.gblearn.com
+
+apt-get install build-essential
+cd /usr/local/lib
+
+  # Get Apache HTTP-server 2.4
+# apache 2.4 comes with trusty by default, but might not have apr/apr-util/pcre?
+wget http://lib.gblearn.com/apache/httpd-2.4.10.tar.gz -q
+tar xfz httpd-2.4.10.tar.gz
+cd httpd-2.4.10/srclib/
+
+  # Download dependencies
+wget http://lib.gblearn.com/apache/apr/apr-1.5.1.tar.gz -q
+wget http://lib.gblearn.com/apache/apr/apr-util-1.5.3.tar.gz -q
+wget http://lib.gblearn.com/apache/pcre/pcre-8.34.tar.gz -q
+
+  # Install and configure APR
+echo 'Installing APR'
+tar xfz ./apr-1.5.1.tar.gz
+cd apr-1.5.1
+./configure
+make --silent
+make install --silent
+cd ../
+
+  # Install and configure APR util
+echo 'Installing APR util'
+tar xfz ./apr-util-1.5.3.tar.gz
+cd apr-util-1.5.3
+./configure --with-apr=/usr/local/apr/bin/apr-1-config
+make --silent
+make install --silent
+cd ../
+
+  # Install and configure PCRE
+echo 'Installing PCRE'
+tar xfz pcre-8.34.tar.gz
+cd pcre-8.34
+./configure --prefix=/usr/local/pcre
+make --silent
+make install --silent
+cd ../..
+
+  # Install and configure Apache HTTPD 2.4
+echo 'Installing Apache'
+./configure --prefix=/usr/local/apache2 --with-pcre=/usr/local/pcre --enable-so
+make --silent
+make install --silent
+cd /usr/local/apache2/conf
+mkdir sites-enabled
+
