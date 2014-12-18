d105e3a83cb9cbadcb9137ae2ef982fbd3472dd0
Raymond Chan <ucamrc@users.noreply.github.com>
2014-12-18 15:26:14 +0000

    Prevent accidental execution with wrong hostname, insist on user supplying it instead of relying on remembering to edit this script

--- setup-via-api.sh
+++ setup-via-api.sh
@@ -1,3 +1,10 @@
+TENANTHOSTNAME=$1
+
+if [[ X"" = X"$TENANTHOSTNAME" ]]; then
+echo "ERROR: Please supply tenant hostname"
+  exit 0;
+fi
+
 ##### Setup Tenant using local-only admin.grasshopper.com #####
 
 # LOGIN
@@ -13,7 +20,7 @@ curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}
 ### assume returns "id":1
 
 # CREATE APP (needs tenantId from above, change host param to match)
-curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/apps -X POST -d 'displayName=myapp&tenantId=1&host=2013.cam.timetable.grasshopper.com&type=timetable'
+curl -b /tmp/curlcookiejar -c /tmp/curlcookiejar -w '\nHTTP STATUS: %{http_code}\nTIME: %{time_total}\n' -e / admin.grasshopper.com/api/apps -X POST -d 'displayName=myapp&tenantId=1&host='$TENANTHOSTNAME'&type=timetable'
 ### assume returns "id":1
 
 # CREATE APP USER (needs appId from above)
